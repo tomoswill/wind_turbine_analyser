20d27e2971ead0502738b8219b375fcae22dfc40
Thomas Williams <37419355+tomoswill@users.noreply.github.com>
2019-12-26 21:45:28 +0000

    1st pass at implementing user login

--- datalog_analyser/app.py
+++ datalog_analyser/app.py
@@ -6,18 +6,25 @@ from pathlib import Path
 
 from flask import Flask, render_template, request, make_response, jsonify, send_from_directory, redirect, url_for
 from flask_caching import Cache
+from flask_login import LoginManager, login_required, UserMixin, current_user, logout_user, login_user
 
 from .datalog_analyser import FutureEnergyDataLogAnalyser
 
 APP = Flask(__name__)
+APP.secret_key = 'super secret string'  # Change this!
+
 cache = Cache(config={'CACHE_TYPE': 'simple', "CACHE_DEFAULT_TIMEOUT": 300})
 cache.init_app(APP)
+login_manager = LoginManager()
+login_manager.init_app(APP)
 
 CVS_DIR = getenv('CVS_DIR')
 TASKS = []
+USERS = {'admin': {'password': '1'}}
 
 
 @APP.route('/')
+@login_required
 @cache.cached(timeout=60)
 def home():
     cvs_filenames = get_raw_cvs_filenames()
@@ -33,6 +40,7 @@ def home():
 
 
 @APP.route('/process_data', methods=['POST'])
+@login_required
 def process_data():
     data = request.form
     source = get_raw_cvs_filenames()
@@ -59,6 +67,7 @@ def process_data():
 
 @APP.route('/tasks', defaults={'task_id': None}, methods=['GET'])
 @APP.route('/tasks/<task_id>', methods=['GET'])
+@login_required
 def tasks(task_id):
     def _task_status(task):
         return {'task_id': task.name, 'complete': not task.isAlive()}
@@ -70,6 +79,7 @@ def tasks(task_id):
 
 
 @APP.route('/download/<path:filename>', methods=['GET', 'POST'])
+@login_required
 def download(filename):
     if filename in get_processed_cvs_filenames():
         return send_from_directory(directory=Path(CVS_DIR), filename=filename, as_attachment=True)
@@ -77,6 +87,7 @@ def download(filename):
 
 
 @APP.route('/remove/<path:filename>', methods=['GET', 'POST'])
+@login_required
 def remove(filename):
     path = Path(CVS_DIR).joinpath(filename)
     if filename in get_processed_cvs_filenames() and path.exists():
@@ -84,6 +95,25 @@ def remove(filename):
         cache.clear()
         return redirect(url_for('home'), code=HTTPStatus.FOUND)
     return make_response('<h1>Not found</h1>', HTTPStatus.NOT_FOUND)
+
+
+@APP.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    email = request.form['email']
+    if request.form['password'] == USERS[email]['password']:
+        user = User()
+        user.id = email
+        login_user(user)
+    return redirect(url_for('home'))
+
+
+@APP.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'), code=HTTPStatus.FOUND)
 
 
 def get_raw_cvs_filenames():
@@ -94,6 +124,41 @@ def get_raw_cvs_filenames():
 def get_processed_cvs_filenames():
     cvs_paths = sorted(Path(CVS_DIR).glob('processed_*.csv'))
     return [path.name for path in cvs_paths]
+
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in USERS:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in USERS:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == USERS[email]['password']
+
+    return user
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return redirect(url_for('login'), code=HTTPStatus.FOUND)
 
 
 if __name__ == '__main__':
